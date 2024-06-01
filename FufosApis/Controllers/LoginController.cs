3bf144372f9b1388809a1b31d14f91e73211a352
LeinerViloria <vilorialeiner2002@gmail.com>
2024-06-01 12:11:25 -0500

    Avances

--- FufosApis/Controllers/LoginController.cs
+++ FufosApis/Controllers/LoginController.cs
@@ -1,6 +1,11 @@
 ﻿using Appointment.SDK.Backend.Controllers;
 using FufosEntities.DTOS;
 using Microsoft.AspNetCore.Mvc;
+using FufosEntities.Entities;
+using FufosEntities.Utilities;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using FufosApis.Utilities;
 
 namespace FufosApis.Controllers
 {
@@ -10,13 +15,39 @@ namespace FufosApis.Controllers
         [HttpPost("/api/login")]
         public IActionResult Login([FromBody] UserDTO User)
         {
+            using(var context = CreateContext())
+            {
+
+            }
             return Ok();
         }
 
         [HttpPost("/api/register")]
         public IActionResult Register([FromBody] UserDTO User)
         {
-            return Ok();
+            var NewUser = new User()
+            {
+                Email = User.Email,
+                Password = User.Password
+            };
+
+            var Errors = new List<ValidationResult>();
+
+            var IsValid = DataAnnotationsValidator.Validate<User>(NewUser, ref Errors);
+
+            if (!IsValid)
+                throw new ArgumentNullException(JsonConvert.SerializeObject(Errors.Select(x => x.ErrorMessage)));
+
+            NewUser.Password = Utils.HashTo256(NewUser.Password, _tokenConfiguration.Salt);
+
+            using (var dbContext = CreateContext())
+            {
+                dbContext.Add(NewUser);
+                dbContext.SaveChanges();
+            }
+
+            var Token = _jWTService.Generate(NewUser);
+            return Ok(Token);
         }
     }
 }
